b5ad1f31dcb6648f13223b9e4810213b135b3289
Gourab Pal <110736631+gourabpal04@users.noreply.github.com>
2025-04-09 21:58:58 +0530

    Create book.entity.ts

---
+++ src/books/entities/book.entity.ts
@@ -0,0 +1,11 @@
+export class Book {
+  id: string;
+  title: string;
+  author: string;
+  category: string;
+  price: number;
+  rating: number;
+  publishedDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
