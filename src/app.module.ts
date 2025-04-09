0736d61237c8fb88adbf0fa5a48bedce53b967cb
Gourab Pal <110736631+gourabpal04@users.noreply.github.com>
2025-04-09 22:25:28 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 @Module({
-  imports: [
-    AuthModule,
-    BooksModule,
-    UsersModule,
-    PrismaModule,
+  imports: [AuthModule, BooksModule, UsersModule],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
   ],
 })
 export class AppModule {}
