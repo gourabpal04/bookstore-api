72be34a6645da3563539a8f4509741acc21f6380
Gourab Pal <110736631+gourabpal04@users.noreply.github.com>
2025-04-09 22:23:00 +0530

    Update prisma.service.ts

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,24 @@
-import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
+
   async onModuleInit() {
     await this.$connect();
+    console.log('[PrismaService] Database connected');
   }
 
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('[PrismaService] Database disconnected');
+  }
+
+  cleanDatabase() {
+    return this.$transaction([
+      this.book.deleteMany(),
+      this.user.deleteMany(),
+    ]);
   }
 }
